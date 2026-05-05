dd5c466f6eb9138835e839f18600709b2bf5275a
ethanteb <ethanteb@gmail.com>
2026-05-05 18:22:51 +0700

    Delete src/evaluation/evaluator.py

--- src/evaluation/evaluator.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import accuracy_score
-import pandas as pd
-
-class Evaluator:
-    #class to evaluate the performance of the model using accuracy score metric
-    def evaluate(self, y_true: pd.DataFrame, y_pred: pd.DataFrame) -> dict:
-        accuracy = accuracy_score(y_true, y_pred)
-        return {"accuracy": accuracy}
