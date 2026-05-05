d9eb59b3c6677372ea0b1c83a97e43677e9ed332
ethanteb <ethanteb@gmail.com>
2026-05-05 16:32:44 +0700

    updated preprocessing class with train-test splitting implemented

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,7 +1,11 @@
-class Preprocessor:
-    def __init__(self):
-        pass
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-    def preprocess(self, data):
-        # Implement preprocessing logic here
-        return data
+class Preprocessor:
+    def __init__(self, target_column: str):
+        self.target_column = target_column
+
+    def split(self, df: pd.DataFrame):
+        X = df.drop(columns=[self.target_column])
+        y = df[self.target_column]
+        return train_test_split(X, y, test_size=0.2, random_state=42)
